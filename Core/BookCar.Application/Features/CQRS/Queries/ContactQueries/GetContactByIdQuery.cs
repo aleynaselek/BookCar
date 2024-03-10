a0ca87fcfac26fe1ff5b222fb2e2d2b9bcf3281e
aleynaflo <selekaleyna@gmail.com>
2024-03-11 00:21:47 +0300

    25-Contact sınıfı işlemleri

---
+++ Core/BookCar.Application/Features/CQRS/Queries/ContactQueries/GetContactByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCar.Application.Features.CQRS.Queries.ContactQueries
+{
+    public class GetContactByIdQuery
+    {
+        public GetContactByIdQuery(int id)
+        {
+            Id = id;
+        }  
+        public int Id { get; set; }
+
+        
+    }
+}
