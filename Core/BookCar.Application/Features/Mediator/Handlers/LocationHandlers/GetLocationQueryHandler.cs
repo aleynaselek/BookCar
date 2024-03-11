2e7bd994fb73befe7a0f39bc0439c2919b13cb9a
aleynaflo <selekaleyna@gmail.com>
2024-03-12 01:47:34 +0300

    36- Location Handler

---
+++ Core/BookCar.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookCar.Application.Features.Mediator.Results.LocationResults;
+using BookCar.Application.Features.Mediator.Queries.LocationQueries;
+using BookCar.Application.Interfaces;
+using BookCar.Domain.Entities;
+
+namespace BookCar.Application.Features.Mediator.Handlers.LocationHandlers
+{
+    public class GetLocationQueryHandler : IRequestHandler<GetLocationQuery, List<GetLocationQueryResult>>
+    {
+        private readonly IRepository<Location> _repository;
+
+        public GetLocationQueryHandler(IRepository<Location> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetLocationQueryResult>> Handle(GetLocationQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetLocationQueryResult
+            {
+                LocationID = x.LocationID,
+                Name = x.Name
+            }).ToList();
+        }
+    }
+}
---
+++ Core/BookCar.Application/Features/Mediator/Queries/LocationQueries/GetLocationQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using BookCar.Application.Features.Mediator.Results.LocationResults;
+
+namespace BookCar.Application.Features.Mediator.Queries.LocationQueries
+{
+    public class GetLocationQuery : IRequest<List<GetLocationQueryResult>>
+    {
+    }
+}
