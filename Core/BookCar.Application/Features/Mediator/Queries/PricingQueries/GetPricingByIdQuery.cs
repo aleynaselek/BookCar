994f48a4df63cd0737f5f43aaf0a4ae0140440ee
aleynaflo <selekaleyna@gmail.com>
2024-03-12 02:04:17 +0300

    38-Pricing İşlemleri

---
+++ Core/BookCar.Application/Features/Mediator/Queries/PricingQueries/GetPricingByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookCar.Application.Features.Mediator.Results.PricingResults;
+
+namespace BookCar.Application.Features.Mediator.Queries.PricingQueries
+{
+    public class GetPricingByIdQuery : IRequest<GetPricingByIdQueryResult>
+    {
+        public GetPricingByIdQuery(int id)
+        {
+            Id = id;
+        }  
+        public int Id { get; set; }
+    }
+}
