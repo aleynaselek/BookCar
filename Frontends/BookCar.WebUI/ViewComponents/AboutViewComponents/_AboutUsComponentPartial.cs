5e69cc7cec09462f3ed05fe0404d21c5886c340b
aleynaflo <selekaleyna@gmail.com>
2024-03-18 02:01:03 +0300

    48. About Sayfası Consume Hazırlığı

---
+++ Frontends/BookCar.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookCar.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _AboutUsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
