3459b26792e4a13a7ba74bc86b02f376e8dc529c
aleynaflo <selekaleyna@gmail.com>
2024-03-19 21:57:31 +0300

    52. Become a Driver Alanı

---
+++ Frontends/BookCar.WebUI/ViewComponents/AboutViewComponents/_BecomeADriverComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using BookCar.Dto.AboutDtos;
+
+namespace BookCar.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _BecomeADriverComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+//https://localhost:7094/api/Abouts
+//DeserializeObject: api türünde gelen veriyi text türünde göstermek istersek
+//SerializeObject: api türünde text türündegelen veriyi text türünde göstermek istersek (ekleme, güncelleme vb.)
+
---
+++ Frontends/BookCar.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using BookCar.Dto.AboutDtos;
+
+namespace BookCar.WebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+       
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client =  _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7094/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode) 
+            { 
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
+//https://localhost:7094/api/Abouts
+//DeserializeObject: api türünde gelen veriyi text türünde göstermek istersek
+//SerializeObject: api türünde text türündegelen veriyi text türünde göstermek istersek (ekleme, güncelleme vb.)
+
