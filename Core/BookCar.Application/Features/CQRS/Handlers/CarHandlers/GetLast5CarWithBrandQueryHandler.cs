81749afea19631f82cd97477c7f1cf338e90dc1e
aleynaflo <selekaleyna@gmail.com>
2024-03-24 00:40:33 +0300

    61. Son 5 Aracın Getirilmesi Api İşlemi

---
+++ Core/BookCar.Application/Features/CQRS/Handlers/CarHandlers/GetLast5CarWithBrandQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using BookCar.Application.Features.CQRS.Results.CarResults;
+using BookCar.Application.Interfaces;
+using BookCar.Application.Interfaces.CarInterfaces;
+using BookCar.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCar.Application.Features.CQRS.Handlers.CarHandlers
+{
+    public class GetLast5CarWithBrandQueryHandler
+    {
+        private readonly ICarRepository _repository;
+
+        public GetLast5CarWithBrandQueryHandler(ICarRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<GetCarWithBrandQueryResult> Handle()
+        {
+            var values =  _repository.GetLast5CarsWithBrand();
+            return values.Select(x => new GetCarWithBrandQueryResult
+            {
+                CarID = x.CarID,
+                BrandID = x.BrandID,
+                BrandName = x.Brand.Name,
+                Model = x.Model,
+                CoverImageUrl = x.CoverImageUrl,
+                Km = x.Km,
+                Transmission = x.Transmission,
+                Seat = x.Seat,
+                Luggage = x.Luggage,
+                Fuel = x.Fuel,
+                BigImageUrl = x.BigImageUrl,
+            }).ToList();
+        }
+    }
+}
