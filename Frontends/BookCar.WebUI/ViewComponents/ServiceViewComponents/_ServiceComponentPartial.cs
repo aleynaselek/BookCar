4d120f791fb1746fc1cec5f0bd12b741f4c08ca1
aleynaflo <selekaleyna@gmail.com>
2024-03-24 01:05:14 +0300

    63. Hizmetlerin Ana Sayfaya Eklenmesi

--- Frontends/BookCar.WebUI/ViewComponents/ServiceViewComponents/_ServiceComponentPartial.cs
+++ Frontends/BookCar.WebUI/ViewComponents/ServiceViewComponents/_ServiceComponentPartial.cs
@@ -1,27 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json; 
-using BookCar.Dto.TestimonialDtos;
+using Newtonsoft.Json;
+using BookCar.Dto.ServiceDtos;
 
-namespace BookCar.WebUI.ViewComponents.TestimonialViewComponents
+namespace BookCar.WebUI.ViewComponents.ServiceViewComponents
 {
-    public class _TestimonialComponentPartial : ViewComponent
+    public class _ServiceComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        public _ServiceComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
-       
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var client =  _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7094/api/Testimonials");
-            if (responseMessage.IsSuccessStatusCode) 
-            { 
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7094/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
                 return View(values);
             }
             return View();
