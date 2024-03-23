d5079beb231a782f5546362da8b8354ffcde70b2
aleynaflo <selekaleyna@gmail.com>
2024-03-23 19:20:54 +0300

    57. Adres Bilgilerinin Listelenmesi

---
+++ Frontends/BookCar.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using BookCar.Dto.CarDtos; 
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BookCar.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Frontends/BookCar.WebUI/ViewComponents/FooterAddressComponents/_FooterAddressComponentPartial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json; 
+using BookCar.Dto.FooterAddressDtos; 
+
+namespace BookCar.WebUI.ViewComponents.FooterAddressComponents
+{
+    public class _FooterAddressComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterAddressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+       
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client =  _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7094/api/FooterAddress");
+            if (responseMessage.IsSuccessStatusCode) 
+            { 
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
+//https://localhost:7094/api/Abouts
+//DeserializeObject: api türünde gelen veriyi text türünde göstermek istersek
+//SerializeObject: api türünde text türündegelen veriyi text türünde göstermek istersek (ekleme, güncelleme vb.)
+
