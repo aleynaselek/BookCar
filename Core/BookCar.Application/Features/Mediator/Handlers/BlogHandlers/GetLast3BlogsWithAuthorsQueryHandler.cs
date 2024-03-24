0ceee404e2b77efb676583f1356311dd7e0a6269
aleynaflo <selekaleyna@gmail.com>
2024-03-24 09:45:30 +0300

    69. Son 3 Blogun Yazarıyla Getirilmesi

---
+++ Core/BookCar.Application/Features/Mediator/Handlers/BlogHandlers/GetLast3BlogsWithAuthorsQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookCar.Application.Features.Mediator.Results.BlogResults;
+using BookCar.Application.Features.Mediator.Queries.BlogQueries;
+using BookCar.Application.Interfaces;
+using BookCar.Application.Interfaces.BlogInterfaces;
+using BookCar.Domain.Entities;
+
+namespace BookCar.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetLast3BlogsWithAuthorsQueryHandler : IRequestHandler<GetLast3BlogsWithAuthorsQuery, List<GetLast3BlogsWithAuthorsQueryResult>>
+    {
+        private readonly IBlogRepository _repository;
+
+        public GetLast3BlogsWithAuthorsQueryHandler(IBlogRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetLast3BlogsWithAuthorsQueryResult>> Handle(GetLast3BlogsWithAuthorsQuery request, CancellationToken cancellationToken)
+        {
+            var values = _repository.GetLast3BlogsWithAuthors();
+            return values.Select(x => new GetLast3BlogsWithAuthorsQueryResult
+            {
+                BlogID = x.BlogID,
+                Title = x.Title,
+                AuthorID = x.AuthorID,
+                AuthorName = x.Author.Name,
+                CoverImageUrl = x.CoverImageUrl,
+                CreatedDate = x.CreatedDate,
+                CategoryID = x.CategoryID
+            }).ToList();
+        }
+    }
+}
---
+++ Infrastructure/BookCar.Persistence/Repositories/BlogRepositories/CarRepository.cs
@@ -0,0 +1,26 @@
+﻿using BookCar.Application.Interfaces.BlogInterfaces;
+using BookCar.Domain.Entities;
+using BookCar.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBlog.Persistence.Repositories.BlogRepositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly CarBookContext _context;
+        public BlogRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+        public List<Blog> GetLast3BlogsWithAuthors()
+        {
+            var values = _context.Blogs.Include(x => x.Author).OrderByDescending(x => x.BlogID).Take(3).ToList();
+            return values;
+        } 
+    }
+}
