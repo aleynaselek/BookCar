fc4ad38a77288daab8183008b988ce6be6a19618
aleynaflo <selekaleyna@gmail.com>
2024-05-03 02:20:22 +0300

    109-Arabaların Admin Panelinde Listelenmesi

--- Frontends/BookCar.WebUI/Controllers/AdminCarController.cs
+++ Frontends/BookCar.WebUI/Controllers/AdminCarController.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookCar.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BookCar.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
-        { 
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminCarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7094/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var result = await responseMessage.Content.ReadAsStringAsync();
+                var cars = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(result);
+                return View(cars);
+            }
             return View();
         }
     }
