c8bcb20ff272f931c99a21666b32dd2ce5c2acee
aleynaflo <selekaleyna@gmail.com>
2024-05-04 01:14:22 +0300

    111-Dropdown Aracına Markaların Getirilmesi

--- Frontends/BookCar.WebUI/Controllers/AdminCarController.cs
+++ Frontends/BookCar.WebUI/Controllers/AdminCarController.cs
@@ -1,5 +1,7 @@
-﻿using BookCar.Dto.CarDtos;
+﻿using BookCar.Dto.BrandDtos;
+using BookCar.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace BookCar.WebUI.Controllers
@@ -26,8 +28,19 @@ namespace BookCar.WebUI.Controllers
     
 
         [HttpGet]
-        public IActionResult CreateCar()
+        public async Task<IActionResult> CreateCar()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7094/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData); 
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.BrandID.ToString()
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
             return View();
         }
 
