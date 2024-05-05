a8ecacf658b8566f770f6d0eecf14fa7fcd6684b
aleynaflo <selekaleyna@gmail.com>
2024-05-06 00:04:47 +0300

    123-Area Ekleme

---
+++ Frontends/BookCar.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookCar.WebUI.Areas.Admin.Controllers
+{
+    public class AdminBannerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
