4d02ce981fdfe658524d2599646ec20a5e2c16d0
aleynaflo <selekaleyna@gmail.com>
2024-05-06 02:09:24 +0300

    130-Yazar Alanı İşlemleri

---
+++ Frontends/BookCar.Dto/AuthorDtos/CreateAuthorDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCar.Dto.AuthorDtos
+{
+    public class CreateAuthorDto
+    { 
+        public string AuthorName { get; set; }
+        public string AuthorImageUrl { get; set; }
+        public string AuthorDescription { get; set; }
+    }
+}
---
+++ Frontends/BookCar.Dto/AuthorDtos/UpdateAuthorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCar.Dto.AuthorDtos
+{
+    public class UpdateAuthorDto
+    {
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorImageUrl { get; set; }
+        public string AuthorDescription { get; set; }
+
+    }
+}
