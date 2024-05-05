e2f502b91118bb90efdfafcf4c6961de0f7a9973
aleynaflo <selekaleyna@gmail.com>
2024-05-06 00:38:57 +0300

    124-Banner Verilerinin Listelenmesi

--- Frontends/BookCar.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
+++ Frontends/BookCar.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookCar.Dto.BannerDtos;
+using BookCar.Dto.BrandDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BookCar.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AdminBannerController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminBannerController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7094/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var result = await responseMessage.Content.ReadAsStringAsync();
+                var Banners = JsonConvert.DeserializeObject<List<ResultBannerDto>>(result);
+                return View(Banners);
+            }
             return View();
         }
+
     }
 }
